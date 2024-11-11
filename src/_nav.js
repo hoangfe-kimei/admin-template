8664c9109004546b2720dabb70351403f25d1ecf
hoangfe-kimei <hoangtv@kimei.vn>
2024-11-11 16:59:40 +0700

    update structure project #1

--- src/_nav.js
+++ src/_nav.js
@@ -47,87 +47,10 @@ const _nav = [
         name: 'Components',
     },
     {
-        component: CNavGroup,
-        name: 'Base',
-        to: '/base',
+        component: CNavItem,
+        name: 'Payments',
+        to: '/payments',
         icon: <CIcon icon={cilPuzzle} customClassName="nav-icon" />,
-        items: [
-            {
-                component: CNavItem,
-                name: 'Accordion',
-                to: '/base/accordion',
-            },
-            {
-                component: CNavItem,
-                name: 'Breadcrumb',
-                to: '/base/breadcrumbs',
-            },
-            {
-                component: CNavItem,
-                name: 'Cards',
-                to: '/base/cards',
-            },
-            {
-                component: CNavItem,
-                name: 'Carousel',
-                to: '/base/carousels',
-            },
-            {
-                component: CNavItem,
-                name: 'Collapse',
-                to: '/base/collapses',
-            },
-            {
-                component: CNavItem,
-                name: 'List group',
-                to: '/base/list-groups',
-            },
-            {
-                component: CNavItem,
-                name: 'Navs & Tabs',
-                to: '/base/navs',
-            },
-            {
-                component: CNavItem,
-                name: 'Pagination',
-                to: '/base/paginations',
-            },
-            {
-                component: CNavItem,
-                name: 'Placeholders',
-                to: '/base/placeholders',
-            },
-            {
-                component: CNavItem,
-                name: 'Popovers',
-                to: '/base/popovers',
-            },
-            {
-                component: CNavItem,
-                name: 'Progress',
-                to: '/base/progress',
-            },
-            {
-                component: CNavItem,
-                name: 'Spinners',
-                to: '/base/spinners',
-            },
-            {
-                component: CNavItem,
-                name: 'Tables',
-                to: '/base/tables',
-            },
-            {
-                component: CNavItem,
-                name: 'Tabs',
-                to: '/base/tabs',
-            },
-            {
-                component: CNavItem,
-                name: 'Tooltips',
-                to: '/base/tooltips',
-            },
-        ],
     },
     {
         component: CNavGroup,
---
+++ src/views/payments/index.jsx
@@ -0,0 +1,5 @@
+const Payment = () => {
+    return <div>PAYMENT</div>;
+};
+
+export default Payment;
